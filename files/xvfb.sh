a9a6bd4975f6def0aa40dffbbebc58e50c3044e5
Alban Andrieu <alban.andrieu@free.fr>
2016-11-02 10:08:34 +0100

    Ubuntu 16.04

--- files/xvfb.sh
+++ files/xvfb.sh
@@ -1,5 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 # {{ ansible_managed }}
+# chkconfig: 235 99 10
+# description: Start or stop the Xvbf server
+#
+### BEGIN INIT INFO
+# Provides: xvbf
+# Required-Start: $network $syslog
+# Required-Stop: $network
+# Default-Start: 2 3 5
+# Default-Stop: 0 1 6
+# Description: Start or stop the Xvbf server
+### END INIT INFO
 
 if [ -z "$1" ]; then
   echo "`basename $0` {start|stop}"
